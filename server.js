152d0f2ef5bde827a6f3bb60bce82f6a674b98ab
mdmusaif <127724721+Md-musaif@users.noreply.github.com>
2023-11-23 16:41:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 //deploying
 import path from "path";
-import { fileURLToPath } from url;
+import { fileURLToPath } from uri;
 
 //configure env
 dotenv.config();
